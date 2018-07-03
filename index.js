3e9c48c9f2e4605f728b020dbc706cb0ad118df1
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-03 15:21:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(s) {
-  return `Happy holidays, ${s}!`
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name) {
